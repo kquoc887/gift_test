13fe6aa3c7273bcb6cca8ba66d551f35f991ddd4
Quốc Khánh <35265408+kquoc887@users.noreply.github.com>
2023-10-21 00:07:44 +0700

    first commit
    demo

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Liêng Thảo';
+const contentCard = 'Chúc mấy người ngày càng xinh đẹp là điều đầu tiên, có sức khỏe tốt, trong tương lai mấy người sẽ đi du lịch được nhiều nơi mà mấy người muốn đi hơn nữa, cười nhiều hơn, ngủ sớm hơn để có thể rãnh đi dạo cuối tuần buổi sáng cùng tui, hi vọng hôm nay điều tốt và đẹp nhất sẽ đến với mấy người và cả những ngày sau này nữa.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
