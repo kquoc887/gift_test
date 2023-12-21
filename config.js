dbf20ecf100af1347adbdaef1ea6b70f3842263f
kody <kquoc887@gmail.com>
2023-12-22 00:23:32 +0700

    commit code

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Bé Bơ';
 const giftUrl = '#';
-const eventName = 'HPBD';
+const eventName = 'HPBD Bé 🥑';
 const titleCard = 'Thu Phương (Bé Bơ)';
-const contentCard = 'Chúc bé Bơ sẽ thành công hơn trong công việc hiện tại bé Bơ vừa mới làm đây, sẽ vui hơn và hạnh phúc hơn, hi vọng sau này sẽ cùng nhau đón sinh nhật nhiều hơn nữa';
+const contentCard = 'Chúc bé Bơ sẽ thành công với shop hiện tại hơn, sẽ vui hơn và hạnh phúc hơn, gặt hái được nhiều mong ước mà mình đã đề ra, sớm thành nữ doanh nhân trẻ, hi vọng sau này sẽ cùng nhau đón sinh nhật nhiều hơn nữa';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
